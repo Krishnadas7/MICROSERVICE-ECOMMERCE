dfd03ad69995a99e0f5242da249cdd014c324c3d
Krishnadas <skrishnadas38@gmail.com>
2024-07-28 14:57:38 +0530

    adding rabbit mq

---
+++ ORDER/src/models/order.js
@@ -0,0 +1,23 @@
+// order.js
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  products: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'products',
+    required: true,
+  }],
+  totalPrice: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, { collection : 'orders' });
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+
+require("dotenv").config();
+
+module.exports = {
+  port: process.env.PORT || 3001,
+  mongoURI: process.env.MONGODB_PRODUCT_URI || "mongodb://localhost/products",
+  rabbitMQURI: process.env.RABBITMQ_URI || "amqp://localhost",
+  exchangeName: "products",
+  queueName: "products_queue",
+  // ... add more configuration variables
+};
